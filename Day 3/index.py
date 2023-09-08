18cc5996fc7442af37cd97ec17b2833c9db1f1a2
Jaymin Darji <jaymindarji2003@gmail.com>
2023-09-08 21:15:30 +0530

    till day 3 done

---
+++ Day 3/index.py
@@ -0,0 +1,13 @@
+# Module in Python
+"""
+    Module is like a Library (package). We are installing and using.
+
+    Two types
+        1. Build In Modules
+        2. External Modules
+
+    -> we are installing External modules using PIP
+        ex.  pip install pandas
+"""
+
+print("print modules")
