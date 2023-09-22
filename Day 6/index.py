4eb99c21769076dd5ba84a646ad37c60fcece580
Jaymin Darji <jaymindarji2003@gmail.com>
2023-09-22 22:28:30 +0530

    lecture done

---
+++ Day 6/index.py
@@ -0,0 +1,16 @@
+# Variables and Datatypes
+a = 10
+print(a)
+b = "jaymin"
+print(b)
+c = None
+print(type(c), c)
+
+"""
+    There are different types of datatypes
+        1. Numeric datatypes ->int, float, complex
+        2. boolean datatypes -> true/false
+        3. text datatypes -> string
+        4. sequence datatypes ->list,tuples,range
+        5. mapping datatypes -> dict
+"""
