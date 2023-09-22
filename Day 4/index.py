6bb72ca111d4dac0a7cded41916b02892cacc7f6
Jaymin Darji <jaymindarji2003@gmail.com>
2023-09-22 21:58:44 +0530

    previous Lecture code

---
+++ Day 4/index.py
@@ -0,0 +1,3 @@
+# First python program
+print("Hellow world!", 7)
+print(5)
---
+++ Day 5/index.py
@@ -0,0 +1,23 @@
+# Day 5 : Commernts , Escap sequences , print statements
+# There are 2 types of comments : single line , multi line
+# single line comment
+"""
+  Multi line comment
+  hello world 
+"""
+print("hello world")
+
+# Escape sequences
+# \n for newline
+print("jaymin \ndarji")
+# /t for tab
+print("Learning python from \t codewithharry")
+# /writeAnything will print
+print('Learning python from "codewithharry" ')
+
+
+# In print statements we have some features
+# 1. sep="anything"  -> add a separator between print statements contents
+# 2. end="anything"  -> add a content to end of the print statement
+print("please learn python", 7, 8, "google", sep="--")
+print("please learn python", 7, 8, "google ", end="PrintIsEnding")
