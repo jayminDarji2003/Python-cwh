fba80ced289ad200d357aefa542a838f5cf1f3bc
Jaymin Darji <jaymindarji2003@gmail.com>
2023-10-25 17:33:29 +0530

    day 14 done

---
+++ Day 14/index.py
@@ -0,0 +1,15 @@
+"""
+    Conditional Expression
+        1. if
+        2. if else
+        3. if else elif
+        4. nested if else elif
+"""
+
+age = int(input("Enter your age : "))
+print("Your age is : ", age)
+
+if age > 18:
+    print("You can drive")
+else:
+    print("You can not drive")
