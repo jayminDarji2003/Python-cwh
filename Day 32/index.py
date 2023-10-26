d73e6da2ab29ee92ce1fa0a7c8e40628102946c4
Jaymin Darji <jaymindarji2003@gmail.com>
2023-10-26 16:57:53 +0530

    day 32 done

---
+++ Day 32/index.py
@@ -0,0 +1,71 @@
+# Set methods
+
+s1 = {1,2,5,6}
+s2 = {3,6,7}
+
+# union()
+# print(s1.union(s2))
+
+# intersection()
+# print(s1.intersection(s2))
+
+# intersection_update()
+# s1.intersection_update(s2)
+# print(s1)
+
+# update()
+# s1.update(s2)
+# print(s1)
+
+# symmetric_difference()
+# print all the elements which are not same in both sets.
+# print(s1.symmetric_difference(s2))
+
+# symmetric_difference_update()
+# s1.symmetric_difference_update(s2)
+# print(s1)
+
+# difference()
+# print(s1.difference(s2))
+
+# difference_update()
+# s1.difference_update(s2)
+# print(s1)
+
+# isdisjoint()
+# print(s1.isdisjoint(s2))
+
+# ss1 = {1,2,3,4,5,6,7,8,9,10}
+# ss2 = {4,7,8}
+
+# issuperset()
+# print(ss1.issuperset(ss2))
+
+# issubset()
+# print(ss2.issubset(ss1))
+
+# ss2 = {4,7,8}
+
+# add()
+# ss2.add(363636)
+# print(ss2)
+
+# remove()
+# ss2.remove(363636)
+# print(ss2)
+
+# discard()
+# ss2.discard(7)
+# print(ss2)
+
+# pop()
+# ss2.pop()
+# print(ss2)
+
+# clear()
+# ss2.clear()
+# print(ss2)
+
+# del
+# del ss2
+# prnt(ss2)
